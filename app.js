f256f3058be097f2f6de94a0ef45e65213533a8d
tharma133 <69578841+tharma133@users.noreply.github.com>
2021-05-19 09:48:26 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const cors = require('cors')
 const path = require('path')
-require('dotenv').config()
+const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
+const hpp = require('hpp')
+const cookieParser = require('cookie-parser')
 
 const userRoutes = require('./routes/userRoutes')
 const questionRoutes = require('./routes/questionRoutes')
@@ -17,17 +21,27 @@ app.use(express.json())
 
 app.use(cors)
 
-app.use('/api/v1/question', questionRoutes)
-app.use('/api/v1/quiz', quizRoutes)
-app.use('/api/v1/user', userRoutes)
-app.use('/api/v1/submit', submitRoutes)
-
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
+
+app.use(helmet())
+
+app.use(cookieParser())
+
+app.use(mongoSanitize())
+
+app.use(xss())
+
+app.use(hpp())
+
+app.use('/api/v1/question', questionRoutes)
+app.use('/api/v1/quiz', quizRoutes)
+app.use('/api/v1/user', userRoutes)
+app.use('/api/v1/submit', submitRoutes)
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404))
